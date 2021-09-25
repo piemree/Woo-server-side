c41da4d99e3770d5d88b2f1627631fb3b4470b75
piemree <pi.emree@gmail.com>
2021-09-25 21:38:31 +0300

    Add auth guard to routes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/users/users.model';
 import { UsersService } from '../users/users.service';
-
+import { compare } from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(private readonly UsersService: UsersService) {}
-  async findOne(username: string): Promise<User | undefined> {
+
+  /* async findOne(username: string): Promise<User | undefined> {
     return await this.UsersService.findByUsername(username);
+  } */
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user: User = await this.UsersService.findByUsername(username);
+
+    if (user) {
+      const check = await compare(password, user.password);
+      if (check) {
+        const { password, ...result } = user;
+        return result;
+      }
+    }
+    return null;
   }
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,14 +1,30 @@
 import * as mongoose from 'mongoose';
+import {genSalt,hash} from 'bcrypt';
 
 export const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: Number, required: true },
+  password: { type: String, required: true },
+});
+
+UserSchema.pre<User>('save', function (next: Function) {
+  const user = this;
+  if (user.password) {
+      genSalt(10, function (err, salt) {
+          if (err) return next(err);
+
+          hash(user.password, salt, (err, hash) => {
+              if (err) return next(err);
+              user.password = hash;
+              next();
+          });
+      })
+  }
 });
 
 export interface User extends mongoose.Document {
   id: string;
   username: string;
   email: string;
-  password: number;
+  password: string;
 }
